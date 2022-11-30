6ca315d48da486d827ec508f137382c6f3bc46f2
joan1906 <joanblanco1906@gmail.com>
2022-11-30 16:19:34 -0400

    conf proyecto nuevo 'miscelaneo'

---
+++ src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+   <p [style.fontSize.px]="tamanio">
+    hola mundo... esta es una etiqueta
+   </p>
+
+   <button class="btn btn-primary" (click)="tamanio = tamanio + 5">
+    <i class="fa fa-plus"></i>
+   </button>
+
+   <button class="btn btn-primary" (click)="tamanio = tamanio - 5">
+    <i class="fa fa-minus"></i>
+   </button>
+  `,
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  tamanio: number = 20;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
