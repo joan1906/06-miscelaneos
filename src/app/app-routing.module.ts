b088b21a49256a4d843d440d1755c82b86dbacd5
joan1906 <joanblanco1906@gmail.com>
2022-12-19 16:19:39 -0400

    agregando rutas hijas en su propio archivo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { USUARIOS_ROUTES } from './components/usuario/usuarios.routes';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  {
+    path: 'usuario/:id',
+    component: UsuarioComponent,
+    children: USUARIOS_ROUTES
+  },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
